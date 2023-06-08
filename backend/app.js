52139de525a83b24eba862f75df6f677dcf3bd55
Mark Ambrocio <wowmarkambro@gmail.com>
2023-06-08 02:31:46 -0600

    dockerize

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,31 @@ const UserRouter = require("./routes/user");
 const TruckRouter = require("./routes/truck");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const path = require("path");
 
 const server = express();
+const __prod__ = process.env.NODE_ENV === "production";
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
-if (process.env.NODE_ENV !== "production") {
-  server.use(morgan("dev"));
+server.use(morgan("dev"));
+
+if (__prod__) {
+  server.use(express.static(path.resolve(__dirname, "dist")));
 }
 
 server.use("/users", UserRouter);
 server.use("/trucks", TruckRouter);
 
-server.get("/health", (req, res) => {
+server.get("/__health", (req, res) => {
   res.status(200).json({ message: "Server up." });
 });
+
+if (__prod__) {
+  server.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  });
+}
 
 module.exports = server;
