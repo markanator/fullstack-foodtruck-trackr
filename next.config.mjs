73a877f5edbf1ed824c45c555a92197f99b08ce4
Mark Ambrocio <wowmarkambro@gmail.com>
2023-06-19 11:58:14 -0600

    prep for appDir

--- next.config.mjs
+++ next.config.mjs
@@ -8,17 +8,17 @@ await import("./src/env.mjs");
 const config = {
   output: 'standalone',
   reactStrictMode: true,
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  images: {
+    domains: ["avatars.githubusercontent.com"],
   },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
 
 export default config;
