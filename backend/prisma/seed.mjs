ee11bcdd21b69789746c4c39d13a498b8809b9af
Mark Ambrocio <wowmarkambro@gmail.com>
2024-01-16 22:39:45 -0700

    add lucia auth

--- backend/prisma/seed.mjs
+++ backend/prisma/seed.mjs
@@ -1,10 +1,15 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient
-// const prisma = require('../data/db.server')
 const { faker } = require('@faker-js/faker')
+const prisma = new PrismaClient
 
 async function seed() {
+  await prisma.menuItem.deleteMany();
+  await prisma.truckStop.deleteMany();
   await prisma.truck.deleteMany();
+  await prisma.favorite.deleteMany();
+  await prisma.file.deleteMany();
+  await prisma.location.deleteMany();
+  await prisma.key.deleteMany();
   await prisma.session.deleteMany();
   await prisma.user.deleteMany();
   await prisma.role.deleteMany();
